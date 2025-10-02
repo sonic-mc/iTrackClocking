cd776706e29b7af750354cb5365d0b2ea3022c84
sonic mc <allysonmachaka@gmail.com>
2025-10-02 13:02:46 +0200

    up

--- database/migrations/2025_09_29_103433_create_audit_logs_table.php
+++ database/migrations/2025_09_29_103433_create_audit_logs_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
                   ->onDelete('set null');
             $table->string('action');
             $table->text('description')->nullable();
-            $table->timestamps(); // includes created_at & updated_at
+            $table->timestamps();
         });
     }
 
